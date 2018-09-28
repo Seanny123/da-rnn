1db1e7915fb62d5ca58cd680779f3ba6d4feded2
Sean Aubin <seanaubin@gmail.com>
2018-09-28 14:10:24 -0400

    made code device agnostic

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import torch
 from torch import nn
 from torch.autograd import Variable
 from torch import optim
-import torch.nn.functional as tf
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -10,121 +9,14 @@ import numpy as np
 import itertools
 
 import utility as util
-
+from modules import Encoder, Decoder
 
 logger = util.setup_log()
 
-use_cuda = torch.cuda.is_available()
-logger.info("Is CUDA available? %s.", use_cuda)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+logger.info("Using computation device %s.", device)
 
 
-class Encoder(nn.Module):
-    def __init__(self, input_size, hidden_size, T, logger):
-        # input size: number of underlying factors (81)
-        # T: number of time steps (10)
-        # hidden_size: dimension of the hidden state
-        super(Encoder, self).__init__()
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.T = T
-
-        self.logger = logger
-
-        self.lstm_layer = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=1)
-        self.attn_linear = nn.Linear(in_features=2 * hidden_size + T - 1, out_features=1)
-
-    def forward(self, input_data):
-        # input_data: batch_size * T - 1 * input_size
-        input_weighted = Variable(input_data.data.new(input_data.size(0), self.T - 1, self.input_size).zero_())
-        input_encoded = Variable(input_data.data.new(input_data.size(0), self.T - 1, self.hidden_size).zero_())
-        # hidden, cell: initial states with dimention hidden_size
-        hidden = self.init_hidden(input_data)  # 1 * batch_size * hidden_size
-        cell = self.init_hidden(input_data)
-        # hidden.requires_grad = False
-        # cell.requires_grad = False
-        for t in range(self.T - 1):
-            # Eqn. 8: concatenate the hidden states with each predictor
-            x = torch.cat((hidden.repeat(self.input_size, 1, 1).permute(1, 0, 2),
-                           cell.repeat(self.input_size, 1, 1).permute(1, 0, 2),
-                           input_data.permute(0, 2, 1)), dim=2)  # batch_size * input_size * (2*hidden_size + T - 1)
-            # Eqn. 9: Get attention weights
-            x = self.attn_linear(x.view(-1, self.hidden_size * 2 + self.T - 1))  # (batch_size * input_size) * 1
-            attn_weights = tf.softmax(
-                x.view(-1, self.input_size))  # batch_size * input_size, attn weights with values sum up to 1.
-            # Eqn. 10: LSTM
-            weighted_input = torch.mul(attn_weights, input_data[:, t, :])  # batch_size * input_size
-            # Fix the warning about non-contiguous memory
-            # see https://discuss.pytorch.org/t/dataparallel-issue-with-flatten-parameter/8282
-            self.lstm_layer.flatten_parameters()
-            _, lstm_states = self.lstm_layer(weighted_input.unsqueeze(0), (hidden, cell))
-            hidden = lstm_states[0]
-            cell = lstm_states[1]
-            # Save output
-            input_weighted[:, t, :] = weighted_input
-            input_encoded[:, t, :] = hidden
-
-        return input_weighted, input_encoded
-
-    def init_hidden(self, x):
-        # No matter whether CUDA is used, the returned variable will have the same type as x.
-        return Variable(x.data.new(1, x.size(0), self.hidden_size).zero_())  # dimension 0 is the batch dimension
-
-
-class Decoder(nn.Module):
-    def __init__(self, encoder_hidden_size, decoder_hidden_size, T, logger):
-        super(Decoder, self).__init__()
-
-        self.T = T
-        self.encoder_hidden_size = encoder_hidden_size
-        self.decoder_hidden_size = decoder_hidden_size
-
-        self.logger = logger
-
-        self.attn_layer = nn.Sequential(nn.Linear(2 * decoder_hidden_size + encoder_hidden_size, encoder_hidden_size),
-                                        nn.Tanh(), nn.Linear(encoder_hidden_size, 1))
-        self.lstm_layer = nn.LSTM(input_size=1, hidden_size=decoder_hidden_size)
-        self.fc = nn.Linear(encoder_hidden_size + 1, 1)
-        self.fc_final = nn.Linear(decoder_hidden_size + encoder_hidden_size, 1)
-
-        self.fc.weight.data.normal_()
-
-    def forward(self, input_encoded, y_history):
-        # input_encoded: batch_size * T - 1 * encoder_hidden_size
-        # y_history: batch_size * (T-1)
-        # Initialize hidden and cell, 1 * batch_size * decoder_hidden_size
-        hidden = self.init_hidden(input_encoded)
-        cell = self.init_hidden(input_encoded)
-        # hidden.requires_grad = False
-        # cell.requires_grad = False
-        for t in range(self.T - 1):
-            # Eqn. 12-13: compute attention weights
-            # batch_size * T * (2*decoder_hidden_size + encoder_hidden_size)
-            x = torch.cat((hidden.repeat(self.T - 1, 1, 1).permute(1, 0, 2),
-                           cell.repeat(self.T - 1, 1, 1).permute(1, 0, 2), input_encoded), dim=2)
-            x = tf.softmax(self.attn_layer(x.view(-1, 2 * self.decoder_hidden_size + self.encoder_hidden_size
-                                                  )).view(-1, self.T - 1))  # batch_size * T - 1, row sum up to 1
-            # Eqn. 14: compute context vector
-            context = torch.bmm(x.unsqueeze(1), input_encoded)[:, 0, :]  # batch_size * encoder_hidden_size
-
-            if t < self.T - 1:
-                # Eqn. 15
-                y_tilde = self.fc(torch.cat((context, y_history[:, t].unsqueeze(1)), dim=1))  # batch_size * 1
-                # Eqn. 16: LSTM
-                self.lstm_layer.flatten_parameters()
-                _, lstm_output = self.lstm_layer(y_tilde.unsqueeze(0), (hidden, cell))
-                hidden = lstm_output[0]  # 1 * batch_size * decoder_hidden_size
-                cell = lstm_output[1]  # 1 * batch_size * decoder_hidden_size
-
-        # Eqn. 22: final output
-        y_pred = self.fc_final(torch.cat((hidden[0], context), dim=1))
-        # self.logger.info("hidden %s context %s y_pred: %s", hidden[0][0][:10], context[0][:10], y_pred[:10])
-        return y_pred
-
-    def init_hidden(self, x):
-        return Variable(x.data.new(1, x.size(0), self.decoder_hidden_size).zero_())
-
-
-# Train the model
 class DaRnn:
 
     def __init__(self, file_data, logger, encoder_hidden_size=64, decoder_hidden_size=64, T=10,
@@ -139,10 +31,10 @@ class DaRnn:
         self.batch_size = batch_size
 
         self.encoder = Encoder(input_size=self.X.shape[1], hidden_size=encoder_hidden_size, T=T,
-                               logger=logger)
+                               logger=logger).to(device)
         self.decoder = Decoder(encoder_hidden_size=encoder_hidden_size,
                                decoder_hidden_size=decoder_hidden_size,
-                               T=T, logger=logger)
+                               T=T, logger=logger).to(device)
 
         if parallel:
             self.encoder = nn.DataParallel(self.encoder)
@@ -223,10 +115,12 @@ class DaRnn:
         self.encoder_optimizer.zero_grad()
         self.decoder_optimizer.zero_grad()
 
-        input_weighted, input_encoded = self.encoder(Variable(torch.from_numpy(X).type(torch.FloatTensor)))
-        y_pred = self.decoder(input_encoded, Variable(torch.from_numpy(y_history).type(torch.FloatTensor)))
+        input_weighted, input_encoded = self.encoder(
+            Variable(torch.from_numpy(X).type(torch.FloatTensor).to(device)))
+        y_pred = self.decoder(input_encoded,
+                              Variable(torch.from_numpy(y_history).type(torch.FloatTensor).to(device)))
 
-        y_true = Variable(torch.from_numpy(y_target).type(torch.FloatTensor))
+        y_true = Variable(torch.from_numpy(y_target).type(torch.FloatTensor)).to(device)
         loss = self.loss_func(y_pred.squeeze(), y_true)
         loss.backward()
 
@@ -261,8 +155,9 @@ class DaRnn:
                     y_history[j, :] = self.y[
                         range(batch_idx[j] + self.train_size - self.T, batch_idx[j] + self.train_size - 1)]
 
-            y_history = Variable(torch.from_numpy(y_history).type(torch.FloatTensor))
-            _, input_encoded = self.encoder(Variable(torch.from_numpy(X).type(torch.FloatTensor)))
+            y_history = Variable(torch.from_numpy(y_history).type(torch.FloatTensor)).to(device)
+            _, input_encoded = self.encoder(
+                Variable(torch.from_numpy(X).type(torch.FloatTensor)).to(device))
             y_pred[i:(i + self.batch_size)] = self.decoder(input_encoded, y_history).cpu().data.numpy()[:, 0]
             i += self.batch_size
 
---
+++ modules.py
@@ -0,0 +1,110 @@
+import torch
+from torch import nn
+from torch.autograd import Variable
+from torch.nn import functional as tf
+
+
+class Encoder(nn.Module):
+    def __init__(self, input_size, hidden_size, T, logger):
+        # input size: number of underlying factors (81)
+        # T: number of time steps (10)
+        # hidden_size: dimension of the hidden state
+        super(Encoder, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.T = T
+
+        self.logger = logger
+
+        self.lstm_layer = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=1)
+        self.attn_linear = nn.Linear(in_features=2 * hidden_size + T - 1, out_features=1)
+
+    def forward(self, input_data):
+        # input_data: batch_size * T - 1 * input_size
+        input_weighted = Variable(input_data.data.new(input_data.size(0), self.T - 1, self.input_size).zero_())
+        input_encoded = Variable(input_data.data.new(input_data.size(0), self.T - 1, self.hidden_size).zero_())
+        # hidden, cell: initial states with dimension hidden_size
+        hidden = self.init_hidden(input_data)  # 1 * batch_size * hidden_size
+        cell = self.init_hidden(input_data)
+        # hidden.requires_grad = False
+        # cell.requires_grad = False
+        for t in range(self.T - 1):
+            # Eqn. 8: concatenate the hidden states with each predictor
+            x = torch.cat((hidden.repeat(self.input_size, 1, 1).permute(1, 0, 2),
+                           cell.repeat(self.input_size, 1, 1).permute(1, 0, 2),
+                           input_data.permute(0, 2, 1)), dim=2)  # batch_size * input_size * (2*hidden_size + T - 1)
+            # Eqn. 9: Get attention weights
+            x = self.attn_linear(x.view(-1, self.hidden_size * 2 + self.T - 1))  # (batch_size * input_size) * 1
+            attn_weights = tf.softmax(
+                x.view(-1, self.input_size))  # batch_size * input_size, attn weights with values sum up to 1.
+            # Eqn. 10: LSTM
+            weighted_input = torch.mul(attn_weights, input_data[:, t, :])  # batch_size * input_size
+            # Fix the warning about non-contiguous memory
+            # see https://discuss.pytorch.org/t/dataparallel-issue-with-flatten-parameter/8282
+            self.lstm_layer.flatten_parameters()
+            _, lstm_states = self.lstm_layer(weighted_input.unsqueeze(0), (hidden, cell))
+            hidden = lstm_states[0]
+            cell = lstm_states[1]
+            # Save output
+            input_weighted[:, t, :] = weighted_input
+            input_encoded[:, t, :] = hidden
+
+        return input_weighted, input_encoded
+
+    def init_hidden(self, x):
+        # No matter whether CUDA is used, the returned variable will have the same type as x.
+        return Variable(x.data.new(1, x.size(0), self.hidden_size).zero_())  # dimension 0 is the batch dimension
+
+
+class Decoder(nn.Module):
+    def __init__(self, encoder_hidden_size, decoder_hidden_size, T, logger):
+        super(Decoder, self).__init__()
+
+        self.T = T
+        self.encoder_hidden_size = encoder_hidden_size
+        self.decoder_hidden_size = decoder_hidden_size
+
+        self.logger = logger
+
+        self.attn_layer = nn.Sequential(nn.Linear(2 * decoder_hidden_size + encoder_hidden_size, encoder_hidden_size),
+                                        nn.Tanh(), nn.Linear(encoder_hidden_size, 1))
+        self.lstm_layer = nn.LSTM(input_size=1, hidden_size=decoder_hidden_size)
+        self.fc = nn.Linear(encoder_hidden_size + 1, 1)
+        self.fc_final = nn.Linear(decoder_hidden_size + encoder_hidden_size, 1)
+
+        self.fc.weight.data.normal_()
+
+    def forward(self, input_encoded, y_history):
+        # input_encoded: batch_size * T - 1 * encoder_hidden_size
+        # y_history: batch_size * (T-1)
+        # Initialize hidden and cell, 1 * batch_size * decoder_hidden_size
+        hidden = self.init_hidden(input_encoded)
+        cell = self.init_hidden(input_encoded)
+        # hidden.requires_grad = False
+        # cell.requires_grad = False
+        for t in range(self.T - 1):
+            # Eqn. 12-13: compute attention weights
+            # batch_size * T * (2*decoder_hidden_size + encoder_hidden_size)
+            x = torch.cat((hidden.repeat(self.T - 1, 1, 1).permute(1, 0, 2),
+                           cell.repeat(self.T - 1, 1, 1).permute(1, 0, 2), input_encoded), dim=2)
+            x = tf.softmax(self.attn_layer(x.view(-1, 2 * self.decoder_hidden_size + self.encoder_hidden_size
+                                                  )).view(-1, self.T - 1))  # batch_size * T - 1, row sum up to 1
+            # Eqn. 14: compute context vector
+            context = torch.bmm(x.unsqueeze(1), input_encoded)[:, 0, :]  # batch_size * encoder_hidden_size
+
+            if t < self.T - 1:
+                # Eqn. 15
+                y_tilde = self.fc(torch.cat((context, y_history[:, t].unsqueeze(1)), dim=1))  # batch_size * 1
+                # Eqn. 16: LSTM
+                self.lstm_layer.flatten_parameters()
+                _, lstm_output = self.lstm_layer(y_tilde.unsqueeze(0), (hidden, cell))
+                hidden = lstm_output[0]  # 1 * batch_size * decoder_hidden_size
+                cell = lstm_output[1]  # 1 * batch_size * decoder_hidden_size
+
+        # Eqn. 22: final output
+        y_pred = self.fc_final(torch.cat((hidden[0], context), dim=1))
+        # self.logger.info("hidden %s context %s y_pred: %s", hidden[0][0][:10], context[0][:10], y_pred[:10])
+        return y_pred
+
+    def init_hidden(self, x):
+        return Variable(x.data.new(1, x.size(0), self.decoder_hidden_size).zero_())
