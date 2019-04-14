4acfe82392c2e292bcd5b3ace345c78fe67849ef
GoshaQ <g.emelyanov@innopolis.ru>
2019-04-14 17:24:09 +0300

    Fix CUDA tensors allocation

--- modules.py
+++ modules.py
@@ -1,7 +1,6 @@
 import torch
 from torch import nn
-from torch.autograd import Variable
-from torch.nn import functional as tf
+from torch.nn import functional as F
 
 
 def init_hidden(x, hidden_size: int):
@@ -9,7 +8,19 @@ def init_hidden(x, hidden_size: int):
     Train the initial value of the hidden state:
     https://r2rt.com/non-zero-initial-states-for-recurrent-neural-networks.html
     """
-    return Variable(torch.zeros(1, x.size(0), hidden_size))
+    hidden = torch.zeros(1, x.size(0), hidden_size)
+    if torch.cuda.is_available():
+        hidden = hidden.cuda()
+
+    return hidden
+
+
+def init_zeros(*sizes):
+    zeros = torch.zeros(*sizes)
+    if torch.cuda.is_available():
+        zeros = zeros.cuda()
+
+    return zeros
 
 
 class Encoder(nn.Module):
@@ -30,8 +41,9 @@ class Encoder(nn.Module):
 
     def forward(self, input_data):
         # input_data: (batch_size, T - 1, input_size)
-        input_weighted = Variable(torch.zeros(input_data.size(0), self.T - 1, self.input_size))
-        input_encoded = Variable(torch.zeros(input_data.size(0), self.T - 1, self.hidden_size))
+        input_weighted = init_zeros(input_data.size(0), self.T - 1, self.input_size)
+        input_encoded = init_zeros(input_data.size(0), self.T - 1, self.hidden_size)
+
         # hidden, cell: initial states with dimension hidden_size
         hidden = init_hidden(input_data, self.hidden_size)  # 1 * batch_size * hidden_size
         cell = init_hidden(input_data, self.hidden_size)
@@ -44,7 +56,7 @@ class Encoder(nn.Module):
             # Eqn. 8: Get attention weights
             x = self.attn_linear(x.view(-1, self.hidden_size * 2 + self.T - 1))  # (batch_size * input_size) * 1
             # Eqn. 9: Softmax the attention weights
-            attn_weights = tf.softmax(x.view(-1, self.input_size), dim=1)  # (batch_size, input_size)
+            attn_weights = F.softmax(x.view(-1, self.input_size), dim=1)  # (batch_size, input_size)
             # Eqn. 10: LSTM
             weighted_input = torch.mul(attn_weights, input_data[:, t, :])  # (batch_size, input_size)
             # Fix the warning about non-contiguous memory
@@ -85,7 +97,7 @@ class Decoder(nn.Module):
         # Initialize hidden and cell, (1, batch_size, decoder_hidden_size)
         hidden = init_hidden(input_encoded, self.decoder_hidden_size)
         cell = init_hidden(input_encoded, self.decoder_hidden_size)
-        context = Variable(torch.zeros(input_encoded.size(0), self.encoder_hidden_size))
+        context = init_zeros(input_encoded.size(0), self.encoder_hidden_size)
 
         for t in range(self.T - 1):
             # (batch_size, T, (2 * decoder_hidden_size + encoder_hidden_size))
@@ -93,7 +105,7 @@ class Decoder(nn.Module):
                            cell.repeat(self.T - 1, 1, 1).permute(1, 0, 2),
                            input_encoded), dim=2)
             # Eqn. 12 & 13: softmax on the computed attention weights
-            x = tf.softmax(
+            x = F.softmax(
                     self.attn_layer(
                         x.view(-1, 2 * self.decoder_hidden_size + self.encoder_hidden_size)
                     ).view(-1, self.T - 1),
